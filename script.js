fedaae43d627c1affaa056122d6e8018111349da
priyad16 <priyadarshinikanchipuram@gmail.com>
2025-05-16 18:09:35 +0530

    add validation feature

---
+++ script.js
@@ -0,0 +1,71 @@
+let nam=document.getElementById('nam');
+let pw=document.getElementById('pw');
+let btn=document.getElementById('btn');
+let emsg=document.getElementById('err-msg');
+let nemsg=document.getElementById('nerr-msg');
+let pemsg=document.getElementById('perr-msg');
+
+
+
+btn.addEventListener('click',(event)=>{
+    event.preventDefault();
+    let n= nam.value.trim();
+    let p=pw.value.trim();
+
+    if(n==="" && p==="" ){
+        emsg.innerText="All fields are required";
+        emsg.classList.add('err');
+        emsg.classList.remove('succ');
+        nemsg.innerText="";
+        pemsg.innerText="";
+    }
+    else if(n===""){
+        nemsg.innerText="Username is required";
+        pemsg.innerText="";
+        emsg.innerText="";
+    }    
+    else if(p===""){
+        pemsg.innerText="Password is required";
+        emsg.innerText="";
+        nemsg.innerText="";
+    } 
+    else if(n!="catstudio" || p!="upskill"){
+        emsg.innerText="Invalid credentials";
+        emsg.classList.add('err');
+        emsg.classList.remove('succ');
+        nemsg.innerText="";
+        pemsg.innerText="";
+
+
+    }   
+
+    else{
+        emsg.innerText="Login Sucess";
+        emsg.classList.add('succ');
+        emsg.classList.remove('err');
+        nemsg.innerText="";
+        pemsg.innerText="";
+    }    
+
+});
+     
+
+
+let eye=document.getElementById('eye');
+let isvis=false;
+eye.addEventListener('click',()=>{
+
+    if(isvis===false)
+    {
+        pw.type="text";
+        eye.classList.remove('fa-eye'); 
+        eye.classList.add('fa-eye-slash');
+        isvis=true;
+    }
+    else{
+        pw.type="password";
+        eye.classList.remove('fa-eye-slash'); 
+        eye.classList.add('fa-eye');
+        isvis=false;
+    }
+})
